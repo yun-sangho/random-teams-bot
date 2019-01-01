d9b21e5bc7f93e743347f96de7da1bc6e1ab9f1f
Sangho <sangho@Sanghos-MacBook-Air.local>
2019-01-01 09:33:08 +0900

    Implement Slack bot createing random temas from the users who added emojis on the message.
    Bot runs using two threads. One is for runing HTTP server on the port, the other is for schedulering to send specific messages to Slack channel.
    Using apscheduler libaray, I can create and handle one more thread easliy.
    
    The senario of Bot is:
    1. Send a message to the channel.
    2. In the channel, some user add emoji to the meesage.
    3. Bot catch that event, and insert tha
    4. After some time, Bot create teams using by persons array.
    5. Let users know the result.

---
+++ bot.py
@@ -0,0 +1,79 @@
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+from apscheduler.schedulers.background import BackgroundScheduler
+from random import sample
+import requests
+import json
+import os
+
+# Set environment variable.
+SLACK_SIGNING_SECRET = os.environ['SLACK_SIGNING_SECRET']
+CHANNEL_WEB_HOOK_URL = os.environ['CHANNEL_WEB_HOOK_URL']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+
+# Headers for API reqeust.
+headers = {'content-type': 'application/json'}
+
+# Array remembering which user add emoji reaction on the message.
+persons = ['person1', 'person2', 'person3', 'person4', 'person5']
+
+def createTeams(persons):
+  temp = persons.copy()
+  teams = {}
+  team1 = sample(temp, len(persons) // 2)
+  for person in team1:
+    temp.remove(person)
+  teams['team1'] = team1
+  teams['team2'] = temp
+  return teams
+    
+# Implement Scheduler
+def sendQuestion():
+  question = {'text': '<!channel>\n 안녕하세요! 점심 드실분은 이모지달아주세요!'}
+  r = requests.post(CHANNEL_WEB_HOOK_URL, data=json.dumps(question), headers=headers)
+  print(r)
+
+def sendPairs():
+  message = '오늘의 팀은!!!\n'
+  
+  if len(persons) <= 5:
+    teams = ' '.join(persons)
+    message = message + teams
+  else:
+    teams = createTeams(persons)
+    team1 = '팀 1 '+ ' '.join(teams['team1']) + '\n'
+    team2 = '팀 2 ' + ' '.join(teams['team2']) + '\n'
+    message = message + team1 + team2
+  
+  payoff = {'text': message}
+  r = requests.post(CHANNEL_WEB_HOOK_URL, data=json.dumps(payoff), headers=headers)
+  print(r)
+
+# Add jobs on the scheulers
+sched = BackgroundScheduler()
+sched.add_job(sendQuestion, 'interval', seconds=10)
+sched.add_job(sendPairs, 'interval', seconds=5)
+sched.start()
+
+# Run Server
+app = Flask(__name__)
+
+# Bind the Events API route to your existing Flask app by passing the server
+# instance as the last param, or with `server=app`.
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, '/slack/events', app)
+
+# Create an event listener for "reaction_added" events and
+# If a user add emoji on the message on specific channel,
+# put user's id in the persons Array.
+@slack_events_adapter.on('reaction_added')
+def reaction_added(event):
+  user = '<@%s>' % event['event']['user']
+  channel = event['event']['item']['channel']
+  if channel == CHANNEL_ID:
+    if user not in persons:
+      persons.append(user)
+  print(persons)
+
+# # Start the server on port 3000
+if __name__ == '__main__':
+  app.run(port=3000)
